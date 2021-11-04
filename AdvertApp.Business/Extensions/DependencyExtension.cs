d5ad3795670ae48681d58e6e965e0a64f2c99160
baskadir <kadirbas09@gmail.com>
2021-11-04 10:35:19 +0300

    Add business and ui layer, create first migration

---
+++ AdvertApp.Business/Extensions/DependencyExtension.cs
@@ -0,0 +1,21 @@
+﻿using AdvertApp.DataAccess.Context;
+using AdvertApp.DataAccess.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdvertApp.Business.Extensions
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AdvertDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("SqlServerConnectionString"));
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
---
+++ AdvertApp.DataAccess/Migrations/20211103145828_InitialCreate.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvertApp.DataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Abouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Description = table.Column<string>(type: "ntext", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Abouts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Advertisements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Description = table.Column<string>(type: "ntext", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advertisements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Definition = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Definition = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MilitaryStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Definition = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MilitaryStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    School = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    GenderId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppUsers_Genders_GenderId",
+                        column: x => x.GenderId,
+                        principalTable: "Genders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdvertisementId = table.Column<int>(type: "int", nullable: false),
+                    AppUserId = table.Column<int>(type: "int", nullable: false),
+                    ApplicationStatusId = table.Column<int>(type: "int", nullable: false),
+                    MilitaryStatusId = table.Column<int>(type: "int", nullable: false),
+                    PostponeEndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AppliedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WorkExperience = table.Column<int>(type: "int", nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CvPath = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Applications_Advertisements_AdvertisementId",
+                        column: x => x.AdvertisementId,
+                        principalTable: "Advertisements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Applications_ApplicationStatuses_ApplicationStatusId",
+                        column: x => x.ApplicationStatusId,
+                        principalTable: "ApplicationStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Applications_AppUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AppUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Applications_MilitaryStatuses_MilitaryStatusId",
+                        column: x => x.MilitaryStatusId,
+                        principalTable: "MilitaryStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppUserRoles",
+                columns: table => new
+                {
+                    AppUserId = table.Column<int>(type: "int", nullable: false),
+                    AppRoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUserRoles", x => new { x.AppUserId, x.AppRoleId });
+                    table.ForeignKey(
+                        name: "FK_AppUserRoles_AppRoles_AppRoleId",
+                        column: x => x.AppRoleId,
+                        principalTable: "AppRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppUserRoles_AppUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AppUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Abouts",
+                columns: new[] { "Id", "Description", "ImagePath", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed leo vel ipsum efficitur accumsan id eget neque. Nam erat libero, viverra vel nibh at, malesuada pulvinar sapien. ", "https://picsum.photos/id/1014/400/400", "İşe alım danışmanlığı" },
+                    { 2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed leo vel ipsum efficitur accumsan id eget neque. Nam erat libero, viverra vel nibh at, malesuada pulvinar sapien. ", "https://picsum.photos/id/1031/400/400", "Dış Kaynak Hizmetler" },
+                    { 3, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed leo vel ipsum efficitur accumsan id eget neque. Nam erat libero, viverra vel nibh at, malesuada pulvinar sapien. ", "https://picsum.photos/id/1025/400/400", "Eğitim Hizmetleri" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AppRoles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "Member" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ApplicationStatuses",
+                columns: new[] { "Id", "Definition" },
+                values: new object[,]
+                {
+                    { 1, "Applied" },
+                    { 2, "InterviewCall" },
+                    { 3, "Negative" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Definition" },
+                values: new object[,]
+                {
+                    { 1, "Male" },
+                    { 2, "Female" },
+                    { 3, "Other" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MilitaryStatuses",
+                columns: new[] { "Id", "Definition" },
+                values: new object[,]
+                {
+                    { 1, "Completed" },
+                    { 2, "Postpone" },
+                    { 3, "Exempt" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_AdvertisementId_AppUserId",
+                table: "Applications",
+                columns: new[] { "AdvertisementId", "AppUserId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_ApplicationStatusId",
+                table: "Applications",
+                column: "ApplicationStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_AppUserId",
+                table: "Applications",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_MilitaryStatusId",
+                table: "Applications",
+                column: "MilitaryStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUserRoles_AppRoleId",
+                table: "AppUserRoles",
+                column: "AppRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUsers_GenderId",
+                table: "AppUsers",
+                column: "GenderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Abouts");
+
+            migrationBuilder.DropTable(
+                name: "Applications");
+
+            migrationBuilder.DropTable(
+                name: "AppUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "Advertisements");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationStatuses");
+
+            migrationBuilder.DropTable(
+                name: "MilitaryStatuses");
+
+            migrationBuilder.DropTable(
+                name: "AppRoles");
+
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+        }
+    }
+}
